ded69674581dd0668c9b7e865b90fe63a5f8ada9
Neil Matheson <neilmatheson@Neils-MacBook-Pro.local>
2016-10-26 03:12:50 -0400

    modified Calc, Test and pom

--- src/com/Calculator/Calculator.java
+++ src/com/Calculator/Calculator.java
@@ -4,9 +4,7 @@ public class Calculator {
 
   private double x,y;
 
-  public Calculator(double x, double y) {
-	  this.x = x;
-	  this.y = y;
+  public Calculator(){
   }
 
   public double add(double x, double y) {
--- src/com/Calculator/TestCalculator.java
+++ src/com/Calculator/TestCalculator.java
@@ -1,9 +1,27 @@
 package com.Calculator;
 
-import junit.framework.Assert;
-import org.junit.Before;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestCalculator{
+
+	@Test
+	public void testAddWithInteger(){
+		
+		Calculator calc = new Calculator();
+		double sum = calc.add(2,5);
+		
+		assertEquals(7.0, sum);
+		
+	}
+	
+	@Test
+	public void testSubWithNegative(){
+		
+		Calculator calc = new Calculator();
+		double remainder = calc.subtract(5.0, 10.0);
+		
+		assertEquals(-5.0, remainder);
+	}
 	
 }
